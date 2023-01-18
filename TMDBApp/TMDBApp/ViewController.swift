b96913e3ef0e2d3127aeab58c3869b8b55ff88f3
Artyom Butorin <butorin.artiom@gmail.com>
2023-01-18 19:34:43 +0300

    - Project created - Removed Storyboard - Added SnapKit & SwiftLint

---
+++ TMDBApp/TMDBApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TMDBApp
+//
+//  Created by Artyom Butorin on 18.01.23.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
