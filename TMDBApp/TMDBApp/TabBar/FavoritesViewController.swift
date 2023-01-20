aa4f5e4f5dd7eb9c31ee719b6a4803d3ea75aeb5
Artyom Butorin <butorin.artiom@gmail.com>
2023-01-20 20:41:39 +0300

    Add launch screen and splash screen Creating tab bar Add "settings" screen with app version description Add "favorites" and "main" view controllers Install Alamofire and Kingfisher pods

---
+++ TMDBApp/TMDBApp/TabBar/FavoritesViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FavoritesViewController.swift
+//  TMDBApp
+//
+//  Created by Artyom Butorin on 20.01.23.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
